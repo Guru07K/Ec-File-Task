de331f78bce17b2e27e21bb0844c7f5b12808209
Gnanaguru K <141668518+Guru07K@users.noreply.github.com>
2024-10-12 13:01:22 +0530

    Added dish file

--- backend/app.js
+++ backend/app.js
@@ -3,16 +3,22 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/userRoute');
 const adminRouter = require('./routes/adminRoute');
+const path = require('path')
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist'))); 
+
 
 app.use('/ecfile/user', authRouter)
 app.use('/ecfile/admin', adminRouter)
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html')); // Adjusted path
+});
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
